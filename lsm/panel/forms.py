07847ee0ae66290d753c3dee99ae642c24787558
Miguel Dorta <dortamiguel@gmail.com>
2016-01-04 23:46:17 +0000

    added new system to detect when a video is finished

--- lsm/panel/forms.py
+++ lsm/panel/forms.py
@@ -4,6 +4,12 @@ from panel.models import Video
 
 
 class TimerForm(forms.ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(TimerForm, self).__init__(*args, **kwargs)
+		self.fields['videourlid'].widget.attrs.update({
+				'required': 'required',
+			})
+
 	class Meta:
 		model = Video
 		fields = ('videourlid',)
