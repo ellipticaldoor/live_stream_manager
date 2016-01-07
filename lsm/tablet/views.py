c5acdd8d92e6a01c9327c2a2054d95bb2a05d4d3
Miguel Dorta <dortamiguel@gmail.com>
2016-01-07 00:35:42 +0000

    now the tablet only shows aired videos

--- lsm/tablet/views.py
+++ lsm/tablet/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.views.generic import ListView
 
 from panel.models import Video
@@ -6,3 +7,8 @@ from panel.models import Video
 class VideoListView(ListView):
 	template_name = 'tablet/base_tablet.html'
 	model = Video
+
+	def get_context_data(self, **kwargs):
+		context = super(VideoListView, self).get_context_data(**kwargs)
+		context['object_list'] = Video.objects.filter(aired_date__lt=datetime.now())
+		return context
