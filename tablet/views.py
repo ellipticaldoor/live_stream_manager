0c19f2235c46ab397b27ffb4743efc1aaef9e1f5
Miguel Dorta <dortamiguel@gmail.com>
2016-01-19 23:26:26 +0000

    new order for new videos on the tablet

--- tablet/views.py
+++ tablet/views.py
@@ -10,5 +10,5 @@ class VideoListView(ListView):
 
 	def get_context_data(self, **kwargs):
 		context = super(VideoListView, self).get_context_data(**kwargs)
-		context['object_list'] = Video.objects.filter(aired_date__lt=timezone.now())
+		context['object_list'] = Video.objects.filter(aired_date__lt=timezone.now()).order_by("-aired_date")
 		return context
